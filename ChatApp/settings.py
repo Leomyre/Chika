5c31f662606f23f6f56f9e64f4aa9c558b6fc538
Ghosth <ghleomyre@gmail.com>
2024-11-26 14:27:27 +0300

    Ready to deplo
    Disable mode dev et update style

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@ao%wx&+t-k$zsjc$4&*ofju9yzv=l$od6be1wime=_7s%fc)0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['chika-gjfv.onrender.com']
